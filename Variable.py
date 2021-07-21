83f77073aeaf24d42f0aa9542459219238d603fb
Ayodeji <Ayodejishote@hotmail.com>
2021-07-21 15:53:53 +0100

    start of the variable branch.
    This is the beginning of the variable branch

---
+++ Variable.py
@@ -0,0 +1,18 @@
+#In python a variable is written as below 
+price = 10 # we are storing information in the variable identifier aka price and the value 10
+print(price)
+# any changes that is made to the price variable will be seen 
+price = 20 
+price = price +2 
+print(price)
+# There are also different numerical values such as floats, or strings 
+tempreture = 4.4 # float 
+
+name = 'Ayodeji' # string 
+
+print(tempreture,name)
+# another data type is booleans which are either true or false and can be used in code statements
+
+is_completed = True
+
+print("The statment is ",is_completed)
