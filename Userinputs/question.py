620272b8354570f8c9536496b041ec0011d2b500
Ayodeji <Ayodejishote@hotmail.com>
2021-07-27 13:44:13 +0100

    first question file creation
    This is the solution to the question of asking a persons name and their favorite colour then printin the message "person likes said colour

---
+++ Userinputs/question.py
@@ -0,0 +1,3 @@
+#This is the solution to the question of asking a persons name and their favorite colour then printin the message "person likes said colour"
+name,colour = input("what is your name"),input("what is your favorite colour ")
+print(name +" likes " + colour)
