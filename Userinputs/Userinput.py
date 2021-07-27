09bc7588237a83ec3f04b350baa087a544993c5d
Ayodeji <Ayodejishote@hotmail.com>
2021-07-27 13:29:25 +0100

    File ordering
    Decided to make sub folders for all the files

---
+++ Userinputs/Userinput.py
@@ -0,0 +1,4 @@
+# The input function prints something in the terminal, then waits for the user to enter a value and what ever the user enters the input function returns 
+name = input('What is your name? ')
+# This is another example of an expression(a piece of code that produces a value )
+print('Hi ' + name )
