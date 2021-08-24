3b05209f327d949d6bfeeccb17d2a3377acb0194
Ayodeji <Ayodejishote@hotmail.com>
2021-08-24 12:05:26 +0100

    Learned how to do int conversions python
    Learned how to do int conversions in line in python

---
+++ Typeconversion/CurrentAge.py
@@ -0,0 +1,6 @@
+# The purpose of this programe is to take in the users birth year and find their age from it
+birth_year = input( 'Birth year: ' )
+print(type(birth_year))
+age = 2021 - int(birth_year)
+print(type(age))
+print(age)
---
+++ Typeconversion/Question.py
@@ -0,0 +1,4 @@
+# Ask the user for thier weight in pounds then convert it to KG and then print it on the terminal
+Weight = input(" What is your weight in pounds: ")
+kg = int(Weight)/2.204
+print(" Your weight in kg is: ", kg)
